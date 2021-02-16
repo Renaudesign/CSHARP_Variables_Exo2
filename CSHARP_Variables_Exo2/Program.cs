e4cca9e30f58d7ac75ae8a6afa8cc60ab2fb5e9f
renaudesign <renaud.sourmail@gmail.com>
2021-02-16 10:13:51 +0100

    exercice 3 terminé

--- CSHARP_Variables_Exo2/Program.cs
+++ CSHARP_Variables_Exo2/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace CSHARP_Variables_Exo2
+namespace CSHARP_Variables_Exo1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int value1 = 14;
+            int value2 = 66;
+            int result = value1 * value2;
+
+            Console.WriteLine("Je prends d'abord le nombre " + value1);
+            Console.WriteLine("je lui multiplie violemment le nombre " + value2);
+            Console.WriteLine("Et PAF ça donne " + result);
+            Console.WriteLine("Donc si je réssume tout ce fatras d'informations. " + value1 + " et " + value2 + " multipliés font " + result + ".");
         }
     }
 }
