91ef98738e13e5bbfbcd724ad2b332f7731f7710
renaudesign <renaud.sourmail@gmail.com>
2021-02-16 15:52:26 +0100

    correction Exo2

--- CSHARP_Variables_Exo2/Program.cs
+++ CSHARP_Variables_Exo2/Program.cs
@@ -8,12 +8,15 @@ namespace CSHARP_Variables_Exo1
         {
             int value1 = 14;
             int value2 = 66;
-            int result = value1 * value2;
+            int value3 = 33;
+            //int result = value1 * value2;
 
-            Console.WriteLine("Je prends d'abord le nombre " + value1);
-            Console.WriteLine("je lui multiplie violemment le nombre " + value2);
-            Console.WriteLine("Et PAF ça donne " + result);
-            Console.WriteLine("Donc si je réssume tout ce fatras d'informations. " + value1 + " et " + value2 + " multipliés font " + result + ".");
+            Console.WriteLine($"Je prends d'abord le nombre {value1}, que j'additionne au {value2}, et je multiplie le tout à {value3}");
+            //Console.WriteLine("Et PAF ça donne " + result);
+            //Console.WriteLine("Donc si je réssume tout ce fatras d'informations. " + value1 + " et " + value2 + " multipliés font " + result + ".");
+            int result = (value1 + value2) * value3;
+
+            Console.WriteLine("Et ça donne " + result);
         }
     }
 }
